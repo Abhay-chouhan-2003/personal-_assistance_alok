0458a8b5ed72a7e6d5a1b956894b39995a40d939
Abhay-chouhan-2003 <chouhanabhay26@gmail.com>
2025-03-22 20:13:13 +0530

    personal assistance alok

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music ={
+     "shiv chalisha" : "https://www.youtube.com/watch?v=fN6ak2P6JAE&pp=ygURc2hpdiBjaGFsaXNhIGZhc3Q%3D",
+     "ye chamak": "https://www.youtube.com/watch?v=nSw3rg5zBis&pp=ygUveWUgY2hhbWFrIHllIGRhbWFrIHNhYiBrdWNoIHNhcmthciB0dW1oaSBzZSBoYWk%3D"
+    
+    }
